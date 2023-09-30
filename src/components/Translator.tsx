ea0db0b33e36fbb471978ed983108afab8174a46
MiracleUFO <miracleufodiama@gmail.com>
2023-09-30 18:02:49 +0100

    feat: <Translator /> wrapper functionality (untested)

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -1,59 +1,57 @@
-import { ReactElement, useEffect } from 'react';
-import ReactDOMServer from 'react-dom/server';
+import {
+  ReactNode,
+  Children,
+  useState,
+  useCallback,
+} from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
-import language from '../types/language';
-import { DEFAULT_PROPS } from '../constants';
 
 import { LanguageProvider } from '../context/languageContext';
 import useTranslation from '../queries/useTranslation';
+import determineRenderedText from '../utils/determineRenderedText';
 
-const queryClient = new QueryClient();
+import language from '../types/language';
+import { DEFAULT_PROPS, DEFAULT_QUERY_OPTIONS } from '../constants';
 
-const Translator = ({ children, to, from } : {
-  children: ReactElement,
+const queryClient = new QueryClient(DEFAULT_QUERY_OPTIONS);
+
+const Translator: { defaultProps: { from: string; to: string; shouldFallback: boolean; }} = ({
+  children,
+  from,
+  to,
+  shouldFallback,
+} : {
+  children: string,
   from?: language,
   to?: language,
+  shouldFallback?: boolean,
 }) => {
-  const htmlString = ReactDOMServer.renderToStaticMarkup(children);
-  const parser = new window.DOMParser();
-  const doc = parser.parseFromString(htmlString, 'text/html');
+  const [textToTranslate, setTextToTranslate] = useState('');
 
-  if (doc.childNodes.length > 1) {
-    throw new Error('Translator wrapper must have one JSX child.');
-  }
+  const {
+    data,
+    isError,
+    isLoading,
+  } = useTranslation(textToTranslate, from, to);
 
-  // eslint-disable-next-line no-undef, no-unused-vars
-  const traverseNodes = (child: ChildNode | null) => {
-    const node = child;
-    // console.log(node);
-    if (node) {
-      if (node.childNodes.length > 0) {
-        for (let i = 0; i < node.childNodes.length; i++) {
-          if (node.childNodes[i]) {
-            const { data } = useTranslation(node.childNodes[i].textContent || '', to, from);
-            if (data) {
-              if (node.childNodes[i].nodeName === '#text') {
-                node.childNodes[i].textContent = data;
-              } else if (node.childNodes[i].childNodes.length) {
-                traverseNodes(node.childNodes[i] as Element);
-              }
-            }
-          }
-        }
-      }
+  const recursivelyTranslate = useCallback((node: ReactNode): ReactNode => {
+    if (typeof node === 'string') {
+      setTextToTranslate(node);
+      const renderedText = determineRenderedText(node, data, shouldFallback, isError, isLoading);
+      return renderedText;
     }
-    // console.log(node);
-    return <>node</>;
-  };
 
-  useEffect(() => {
-  }, []);
+    if (Children.count(node) === 0) {
+      return node;
+    }
+
+    return Children.map(node, (child) => recursivelyTranslate(child));
+  }, [data, isError, isLoading]);
 
   return (
     <QueryClientProvider client={queryClient}>
       <LanguageProvider>
-        Bonjour
+        {recursivelyTranslate(children)}
       </LanguageProvider>
     </QueryClientProvider>
   );
